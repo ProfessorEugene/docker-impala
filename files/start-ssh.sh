b5f712b2e6974e1669ecdc6aa3e523575084ef4a
Phillip Cloud <cpcloud@gmail.com>
2017-02-14 20:27:36 -0500

    Do it anyways

--- files/start-ssh.sh
+++ files/start-ssh.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-if [ ! -e "/home/dev/.ssh/id_rsa" ]; then
-  echo "Generating an SSH key for the dev user"
-  sudo -u dev mkdir -p /home/dev/.ssh
-  sudo -u dev ssh-keygen -t rsa -q -N "" -f /home/dev/.ssh/id_rsa
-  sudo -u dev cp /home/dev/.ssh/{id_rsa.pub,authorized_keys}
-  sudo -u dev chmod 600 /home/dev/.ssh/authorized_keys
-fi
+sudo -u dev mkdir -p /home/dev/.ssh
+sudo -u dev ssh-keygen -t rsa -q -N "" -f /home/dev/.ssh/id_rsa
+sudo -u dev cp /home/dev/.ssh/{id_rsa.pub,authorized_keys}
+sudo -u dev chmod 600 /home/dev/.ssh/authorized_keys
 sudo /etc/init.d/ssh start
