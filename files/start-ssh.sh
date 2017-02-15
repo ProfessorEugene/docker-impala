df6324faab449509534e31b2967e7da608baa047
Phillip Cloud <cpcloud@gmail.com>
2017-02-14 21:21:39 -0500

    Start SSH with the ENTRYPOINT command

--- files/start-ssh.sh
+++ files/start-ssh.sh
@@ -4,4 +4,3 @@ sudo -u dev mkdir -p /home/dev/.ssh
 sudo -u dev ssh-keygen -t rsa -q -N "" -f /home/dev/.ssh/id_rsa
 sudo -u dev cp /home/dev/.ssh/{id_rsa.pub,authorized_keys}
 sudo -u dev chmod 600 /home/dev/.ssh/authorized_keys
-sudo /etc/init.d/ssh start
