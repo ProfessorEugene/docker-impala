791d395e9b546ca25b218487a081e5f93b4b66d3
Phillip Cloud <cpcloud@gmail.com>
2017-02-14 19:28:37 -0500

    Sudo-ize files/start-hdfs.sh

--- files/start-hdfs.sh
+++ files/start-hdfs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sed -i "s/__HOSTNAME__/$(hostname -i)/" /etc/hadoop/conf/core-site.xml
-sed -i "s/__HOSTNAME__/$(hostname -i)/" /etc/impala/conf/core-site.xml
+sudo sed -i "s/__HOSTNAME__/$(hostname -i)/" /etc/hadoop/conf/core-site.xml
+sudo sed -i "s/__HOSTNAME__/$(hostname -i)/" /etc/impala/conf/core-site.xml
 if [[ ! -e /var/lib/hadoop-hdfs/cache/hdfs/dfs/name/current ]]; then
-	/etc/init.d/hadoop-hdfs-namenode init
+	sudo /etc/init.d/hadoop-hdfs-namenode init
 fi
-/etc/init.d/hadoop-hdfs-namenode start
-/etc/init.d/hadoop-hdfs-datanode start
+sudo /etc/init.d/hadoop-hdfs-namenode start
+sudo /etc/init.d/hadoop-hdfs-datanode start
