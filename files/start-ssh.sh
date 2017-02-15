57890013a9a8ad7edc5aaeb1f25beb8a2c6ce86a
Phillip Cloud <cpcloud@gmail.com>
2017-02-14 19:29:22 -0500

    Create new ssh keys as dev

--- files/start-ssh.sh
+++ files/start-ssh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rm -rf /.ssh
-echo "Generating an SSH key for the dev user"
-mkdir -p /.ssh
-ssh-keygen -t rsa -q -N "" -f /.ssh/id_rsa
-cp /.ssh/{id_rsa.pub,authorized_keys}
-chmod 600 /.ssh/authorized_keys
+if [ ! -e "/home/dev/.ssh/id_rsa" ]; then
+  echo "Generating an SSH key for the dev user"
+  sudo -u dev mkdir -p /home/dev/.ssh
+  sudo -u dev ssh-keygen -t rsa -q -N "" -f /home/dev/.ssh/id_rsa
+  sudo -u dev cp /home/dev/.ssh/{id_rsa.pub,authorized_keys}
+  sudo -u dev chmod 600 /home/dev/.ssh/authorized_keys
+fi
+sudo /etc/init.d/ssh start
