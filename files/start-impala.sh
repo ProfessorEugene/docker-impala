77b1970f6bb8f7f65a41954cf2ae6a4d02a530f6
Phillip Cloud <cpcloud@gmail.com>
2017-02-15 21:26:20 -0500

    Wait for impala to become queryable

--- files/start-impala.sh
+++ files/start-impala.sh
@@ -11,3 +11,21 @@ sudo -u hdfs hdfs dfs -chown dev:dev /user/dev
 sudo /etc/init.d/impala-catalog start
 sudo /etc/init.d/impala-state-store start
 sudo /etc/init.d/impala-server start
+
+export PYTHON_EGG_CACHE=/home/dev/.python-eggs
+mkdir -p "${PYTHON_EGG_CACHE}"
+
+chars="/-\|"
+
+start=$(date +%s)
+
+while ! sudo -E -u dev impala-shell -i impala -q 'select 1'; do
+  for (( i=0; i<${#chars}; i++ )); do
+    sleep 0.5
+    echo -en "${chars:$i:1}" "\r"
+  done
+done
+
+stop=$(date +%s)
+
+echo "Impala took $(($stop - $start)) seconds to become queryable"
