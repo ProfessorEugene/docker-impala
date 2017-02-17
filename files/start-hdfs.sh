7f95e9ca7ad6bc83b52eae04cf6551f9b50a841f
Phillip Cloud <cpcloud@gmail.com>
2017-02-17 14:51:57 -0500

    Replace __HOSTNAME__ with actual hostname

--- files/start-hdfs.sh
+++ files/start-hdfs.sh
@@ -3,6 +3,7 @@
 declare -a hostname=($(hostname -i))
 sudo sed -i "s/__HOSTNAME__/${hostname[0]}/" /etc/hadoop/conf/core-site.xml
 sudo sed -i "s/__HOSTNAME__/${hostname[0]}/" /etc/impala/conf/core-site.xml
+sudo sed -i "s/__HOSTNAME__/${hostname[0]}/" /etc/hive/conf.dist/hive-site.xml
 if [[ ! -e /var/lib/hadoop-hdfs/cache/hdfs/dfs/name/current ]]; then
 	sudo /etc/init.d/hadoop-hdfs-namenode init
 fi
